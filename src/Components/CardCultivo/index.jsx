be38cd829c2feb1dc58b0fb8ec8531241103172a
santiagoSuarez219 <santiago8628@gmail.com>
2023-12-07 18:13:39 -0500

    Add MQTT dependency and update CardCultivo component

--- src/Components/CardCultivo/index.jsx
+++ src/Components/CardCultivo/index.jsx
@@ -1,26 +1,108 @@
+import React, { useState, useEffect } from "react";
 import { WiThermometer } from "react-icons/wi";
 import { WiRaindrop } from "react-icons/wi";
 import { MdCo2 } from "react-icons/md";
 import { WiDaySunny } from "react-icons/wi";
-import VariableSensor from "../VariableSensor"
 import { WiHumidity } from "react-icons/wi";
 import { PiPlant } from "react-icons/pi";
+import mqtt from "mqtt";
+import VariableSensor from "../VariableSensor";
 
 const CardCultivo = () => {
-    return (
-        <div className='flex flex-col p-2 shadow'>
-            <div className='flex justify-between'>
-                <p className='font-bold text-base'>Cultivo</p>
-                <PiPlant className='text-green-900' />
-            </div>
-            <VariableSensor parametro={'Temperatura'} valor = {30} icon={ WiThermometer } iconColor={'text-green-900'} unidades={'°C'} />
-            <VariableSensor parametro={'Humedad'} valor = {100} icon={ WiRaindrop } iconColor={'text-blue-900'} unidades={'%'} />
-            <VariableSensor parametro={'Dioxido de Carbono'} valor = {100} icon={ MdCo2 } iconColor={'text-green-900'} unidades={'ppm'} />
-            <VariableSensor parametro={'PPFD'} valor = {2} icon={ WiDaySunny } iconColor={'text-yellow-500'} unidades={'umol/m2'} />
-            <VariableSensor parametro={'PPF'} valor = {2} icon={ WiDaySunny } iconColor={'text-yellow-500'} unidades={'umol'} />
-            <VariableSensor parametro={'PPFD'} valor = {2} icon={ WiHumidity } iconColor={'text-blue-500'} unidades={'%'} />
-        </div>
-    )
-}
+  const [client, setClient] = useState(null);
+  const [temperatura, setTemperatura] = useState(null);
+  const [humedad, setHumedad] = useState(null);
+  const [co2, setCo2] = useState(null);
+  const [ppf, setPpf] = useState(null);
+  const [ppfd, setPpfd] = useState(null);
+  const [vpd, setVpd] = useState(null);
 
-export default CardCultivo
+  useEffect(() => {
+    const connectToMqttBroker = () => {
+      const mqttBrokerUrl = "ws://localhost:8083/mqtt";
+      const mqttClient = mqtt.connect(mqttBrokerUrl);
+
+      mqttClient.on("connect", () => {
+        console.log("Conectado al broker MQTT");
+        mqttClient.subscribe("smartgrow");
+      });
+
+      mqttClient.on("message", (topic, message) => {
+        console.log(message.toString());
+        if (topic === "smartgrow") {
+          setTemperatura(JSON.parse(message.toString()).temperatura);
+          setHumedad(JSON.parse(message.toString()).humedad);
+          setCo2(JSON.parse(message.toString()).co2);
+          setPpf(JSON.parse(message.toString()).PPF);
+          setPpfd(JSON.parse(message.toString()).PPFD);
+          setVpd(JSON.parse(message.toString()).VPD);
+        }
+      });
+
+      setClient(mqttClient);
+    };
+
+    if (!client) {
+      connectToMqttBroker();
+    }
+
+    return () => {
+      if (client) {
+        client.end();
+      }
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col p-2 shadow">
+      <div className="flex justify-between">
+        <p className="font-bold text-base">Cultivo</p>
+        <PiPlant className="text-green-900" />
+      </div>
+      <VariableSensor
+        parametro={"Temperatura"}
+        valor={temperatura !== null ? temperatura : "Cargando..."}
+        icon={WiThermometer}
+        iconColor={"text-green-900"}
+        unidades={"°C"}
+      />
+      <VariableSensor
+        parametro={"Humedad"}
+        valor={humedad !== null ? humedad : "Cargando..."}
+        icon={WiRaindrop}
+        iconColor={"text-blue-900"}
+        unidades={"%"}
+      />
+      <VariableSensor
+        parametro={"Dioxido de Carbono"}
+        valor={co2 !== null ? co2 : "Cargando..."}
+        icon={MdCo2}
+        iconColor={"text-green-900"}
+        unidades={"ppm"}
+      />
+      <VariableSensor
+        parametro={"PPFD"}
+        valor={ppfd !== null ? ppfd : "Cargando..."}
+        icon={WiDaySunny}
+        iconColor={"text-yellow-500"}
+        unidades={"umol/m2"}
+      />
+      <VariableSensor
+        parametro={"PPF"}
+        valor={ppf !== null ? ppf : "Cargando..."}
+        icon={WiDaySunny}
+        iconColor={"text-yellow-500"}
+        unidades={"umol"}
+      />
+      <VariableSensor
+        parametro={"VPD"}
+        valor={vpd !== null ? vpd : "Cargando..."}
+        icon={WiHumidity}
+        iconColor={"text-blue-500"}
+        unidades={"%"}
+      />
+    </div>
+  );
+};
+
+export default CardCultivo;
--- src/Components/VariableSensor/index.jsx
+++ src/Components/VariableSensor/index.jsx
@@ -1,12 +1,14 @@
-const VariableSensor = ({ parametro, valor, icon: Icon, iconColor, unidades }) => {
+const VariableSensor = ({ parametro, icon: Icon, iconColor, unidades, valor }) => {
   return (
-    <div className='flex justify-between p-1 mt-2'>
-      <div className='flex items-center'>
+    <div className="flex justify-between p-1 mt-2">
+      <div className="flex items-center">
         <Icon className={`w-8 h-8 ${iconColor}`} />
-        <p className='ml-2'>{parametro}</p>
+        <p className="ml-2">{parametro}</p>
       </div>
-      <div className='flex'>
-        <p>{valor} {unidades}</p>
+      <div className="flex">
+        <p>
+          {valor} {unidades}
+        </p>
       </div>
     </div>
   );
