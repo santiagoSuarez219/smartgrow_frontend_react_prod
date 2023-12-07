e23c76214d1ca2590781e45f5d8207fae221ba64
santiagoSuarez219 <santiago8628@gmail.com>
2023-12-07 18:36:54 -0500

    Refactor CardCultivo component and add MQTT connection to Home page

--- src/Components/CardCultivo/index.jsx
+++ src/Components/CardCultivo/index.jsx
@@ -1,58 +1,19 @@
-import React, { useState, useEffect } from "react";
 import { WiThermometer } from "react-icons/wi";
 import { WiRaindrop } from "react-icons/wi";
 import { MdCo2 } from "react-icons/md";
 import { WiDaySunny } from "react-icons/wi";
 import { WiHumidity } from "react-icons/wi";
 import { PiPlant } from "react-icons/pi";
-import mqtt from "mqtt";
 import VariableSensor from "../VariableSensor";
 
-const CardCultivo = () => {
-  const [client, setClient] = useState(null);
-  const [temperatura, setTemperatura] = useState(null);
-  const [humedad, setHumedad] = useState(null);
-  const [co2, setCo2] = useState(null);
-  const [ppf, setPpf] = useState(null);
-  const [ppfd, setPpfd] = useState(null);
-  const [vpd, setVpd] = useState(null);
-
-  useEffect(() => {
-    const connectToMqttBroker = () => {
-      const mqttBrokerUrl = "ws://localhost:8083/mqtt";
-      const mqttClient = mqtt.connect(mqttBrokerUrl);
-
-      mqttClient.on("connect", () => {
-        console.log("Conectado al broker MQTT");
-        mqttClient.subscribe("smartgrow");
-      });
-
-      mqttClient.on("message", (topic, message) => {
-        console.log(message.toString());
-        if (topic === "smartgrow") {
-          setTemperatura(JSON.parse(message.toString()).temperatura);
-          setHumedad(JSON.parse(message.toString()).humedad);
-          setCo2(JSON.parse(message.toString()).co2);
-          setPpf(JSON.parse(message.toString()).PPF);
-          setPpfd(JSON.parse(message.toString()).PPFD);
-          setVpd(JSON.parse(message.toString()).VPD);
-        }
-      });
-
-      setClient(mqttClient);
-    };
-
-    if (!client) {
-      connectToMqttBroker();
-    }
-
-    return () => {
-      if (client) {
-        client.end();
-      }
-    };
-  }, []);
-
+const CardCultivo = ({
+  temperatura,
+  humedad,
+  co2,
+  ppf,
+  ppfd,
+  vpd
+}) => {
   return (
     <div className="flex flex-col p-2 shadow">
       <div className="flex justify-between">
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,54 @@
+import React, { useState, useEffect } from "react";
+import mqtt from "mqtt";
 import Navbar from '../../Components/Navbar'
 import Connections from '../../Components/Connections/Connections'
 import LayoutCards from '../../Components/LayoutCards'
 import CardCultivo from '../../Components/CardCultivo'
 
 function Home() {
+  const [client, setClient] = useState(null);
+  const [temperatura, setTemperatura] = useState(null);
+  const [humedad, setHumedad] = useState(null);
+  const [co2, setCo2] = useState(null);
+  const [ppf, setPpf] = useState(null);
+  const [ppfd, setPpfd] = useState(null);
+  const [vpd, setVpd] = useState(null);
+
+  useEffect(() => {
+    const connectToMqttBroker = () => {
+      const mqttBrokerUrl = "ws://localhost:8083/mqtt";
+      const mqttClient = mqtt.connect(mqttBrokerUrl);
+
+      mqttClient.on("connect", () => {
+        console.log("Conectado al broker MQTT");
+        mqttClient.subscribe("smartgrow");
+      });
+
+      mqttClient.on("message", (topic, message) => {
+        if (topic === "smartgrow") {
+          setTemperatura(JSON.parse(message.toString()).temperatura);
+          setHumedad(JSON.parse(message.toString()).humedad);
+          setCo2(JSON.parse(message.toString()).co2);
+          setPpf(JSON.parse(message.toString()).PPF);
+          setPpfd(JSON.parse(message.toString()).PPFD);
+          setVpd(JSON.parse(message.toString()).VPD);
+        }
+      });
+
+      setClient(mqttClient);
+    };
+
+    if (!client) {
+      connectToMqttBroker();
+    }
+
+    return () => {
+      if (client) {
+        client.end();
+      }
+    };
+  }, []);
+
   return (
     <div>
        <Navbar/>
@@ -11,7 +56,14 @@ function Home() {
          <Connections/>
        </div>
        <LayoutCards>
-        <CardCultivo />
+       <CardCultivo
+          temperatura={temperatura}
+          humedad={humedad}
+          co2={co2}
+          ppf={ppf}
+          ppfd={ppfd}
+          vpd={vpd}
+        />
        </LayoutCards>
     </div>
   )
