f83c968d4194c1dd44bbdd56d7072e5d00f755de
santiagoSuarez219 <santiago8628@gmail.com>
2023-12-11 16:05:14 -0500

    Add useMqtt hook for MQTT functionality

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,53 +1,11 @@
-import React, { useState, useEffect } from "react";
-import mqtt from "mqtt";
 import Navbar from '../../Components/Navbar'
 import Connections from '../../Components/Connections/Connections'
 import LayoutCards from '../../Components/LayoutCards'
 import CardCultivo from '../../Components/CardCultivo'
+import { useMqtt } from "./useMqtt";
 
 function Home() {
-  const [client, setClient] = useState(null);
-  const [temperatura, setTemperatura] = useState(null);
-  const [humedad, setHumedad] = useState(null);
-  const [co2, setCo2] = useState(null);
-  const [ppf, setPpf] = useState(null);
-  const [ppfd, setPpfd] = useState(null);
-  const [vpd, setVpd] = useState(null);
-
-  useEffect(() => {
-    const connectToMqttBroker = () => {
-      const mqttBrokerUrl = "ws://localhost:8083/mqtt";
-      const mqttClient = mqtt.connect(mqttBrokerUrl);
-
-      mqttClient.on("connect", () => {
-        console.log("Conectado al broker MQTT");
-        mqttClient.subscribe("smartgrow");
-      });
-
-      mqttClient.on("message", (topic, message) => {
-        if (topic === "smartgrow") {
-          setTemperatura(JSON.parse(message.toString()).temperatura);
-          setHumedad(JSON.parse(message.toString()).humedad);
-          setCo2(JSON.parse(message.toString()).co2);
-          setPpf(JSON.parse(message.toString()).PPF);
-          setPpfd(JSON.parse(message.toString()).PPFD);
-          setVpd(JSON.parse(message.toString()).VPD);
-        }
-      });
-
-      setClient(mqttClient);
-    };
-
-    if (!client) {
-      connectToMqttBroker();
-    }
-
-    return () => {
-      if (client) {
-        client.end();
-      }
-    };
-  }, []);
+  const { temperatura, humedad, co2, ppf, ppfd, vpd } = useMqtt();
 
   return (
     <div>
